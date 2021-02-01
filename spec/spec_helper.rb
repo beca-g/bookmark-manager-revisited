c0f402229e1ed937f6a94b1b8bf7f420068d4ded
Beca-g <rebekahgalliano@gmail.com>
2021-02-01 13:42:31 +0000

    Setup ENV - Test DB will truncate after each test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,17 @@
-ENV['RACK_ENV'] = 'test'
+ENV['ENVIRONMENT'] = 'test'
+
+RSpec.configure do |config|
+  config.before(:each) do
+    setup_test_database
+  end
+end
 
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
 
 require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
+require_relative './setup_test_database.rb'
 require 'simplecov'
 require 'simplecov-console'
 
