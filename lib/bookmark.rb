94c179e3b1d3bb3be13b17c51980c27b54830a71
Beca-g <rebekahgalliano@gmail.com>
2021-02-01 11:40:18 +0000

    Set up DB connection
    Added "pg" to the gem file and established a connection to the bookmarks DB in the bookmarks class.

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,11 +1,10 @@
+require "pg"
+
 class Bookmark
 
   def self.all
-    [
-      "http://www.asos.com",
-      "http://www.rightmove.com",
-      "http://www.cultbeauty.co.uk"
-    ]
+    connection = PG.connect(dbname: "bookmarks")
+    result = connection.exec("SELECT * FROM bookmarks;")
+    result.map { |bookmark| bookmark["url"] }
   end
-
 end
