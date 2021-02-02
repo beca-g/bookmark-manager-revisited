3206e90e9ff103e1753b2f92edf372f164cae6b3
Beca-g <rebekahgalliano@gmail.com>
2021-02-02 12:16:37 +0000

    Updated adding a bookmark feature test to contain title
    Bookmarks are now shown as links on the page

--- spec/features/adding_bookmarks_spec.rb
+++ spec/features/adding_bookmarks_spec.rb
@@ -2,8 +2,10 @@ feature "adding bookmarks" do
   scenario "a user can save a new bookmark" do
     visit("/bookmarks/new")
     fill_in("url", with: "http://www.test.com")
+    fill_in("title", with: "Test")
     click_button("Save")
+    visit("/bookmarks")
 
-    expect(page).to have_content "http://www.test.com"
+    expect(page).to have_link("Test", href: "http://www.test.com")
   end  
 end
