1593846ee348b6b018d7b150307102a989e1fe42
Beca-g <rebekahgalliano@gmail.com>
2021-02-02 12:18:35 +0000

    Updated viewing bookmarks feat test to pass with a title as a link
     Title: Summary, imperative, start upper case, don't end with a period

--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -1,13 +1,13 @@
 feature "viewing bookmarks" do
   scenario "a user can see bookmarks" do
-    Bookmark.create(url: "http://www.asos.com")
-    Bookmark.create(url: "http://www.rightmove.com")
-    Bookmark.create(url: "http://www.cultbeauty.co.uk")
+    Bookmark.create(url: "http://www.asos.com", title: "ASOS")
+    Bookmark.create(url: "http://www.rightmove.com", title: "Right Move")
+    Bookmark.create(url: "http://www.cultbeauty.co.uk", title: "Cult Beauty")
 
     visit('/bookmarks')
 
-    expect(page).to have_content "http://www.asos.com"
-    expect(page).to have_content "http://www.rightmove.com"
-    expect(page).to have_content "http://www.cultbeauty.co.uk"
+    expect(page).to have_link("ASOS", href: "http://www.asos.com")
+    expect(page).to have_link("Right Move", href: "http://www.rightmove.com")
+    expect(page).to have_link("Cult Beauty", href: "http://www.cultbeauty.co.uk")
   end
 end
